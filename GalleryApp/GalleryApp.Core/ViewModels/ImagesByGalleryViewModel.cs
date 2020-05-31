baea864705c5eff8ce2a3af610cc2e207440bb30
Erman Kemanci <ekemanci01@hotmail.com>
2020-05-31 18:57:24 +0100

    Listed Images By Gallery

---
+++ GalleryApp/GalleryApp.Core/ViewModels/ImagesByGalleryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GalleryApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalleryApp.Core.ViewModels
+{
+    public class ImagesByGalleryViewModel
+    {
+       
+            public IEnumerable<Gallery> Galleries { get; set; }
+            public IEnumerable<GalleryImage> GalleryImages { get; set; }
+
+        
+    }
+}
--- GalleryApp/GalleryApp.MVCUI/Controllers/HomeController.cs
+++ GalleryApp/GalleryApp.MVCUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GalleryApp.Core.Contracts;
+using GalleryApp.Core.Models;
+using GalleryApp.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +11,38 @@ namespace GalleryApp.MVCUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IRepository<Gallery> _galleryRepository;
+        IRepository<GalleryImage> _galleryImageRespository;
+        public HomeController(IRepository<Gallery> galleryRepository, IRepository<GalleryImage> galleryImageRespository)
         {
-            return View();
+            _galleryImageRespository = galleryImageRespository;
+            _galleryRepository = galleryRepository;
         }
+        public ActionResult Index(string GalleryName = null)
+        {
+          
+            List<GalleryImage> galleryImageList;
+            List<Gallery> galleryList = _galleryRepository.Collection().ToList();
 
+            if (GalleryName == null)
+            {
+                galleryImageList = _galleryImageRespository.Collection().ToList();
+               
+            }
+            else
+            {
+                galleryImageList = _galleryImageRespository.Collection().Where(x=>x.GalleryName== GalleryName).ToList();
+
+            }
+            ImagesByGalleryViewModel viewModel = new ImagesByGalleryViewModel()
+            {
+                Galleries = galleryList,
+                GalleryImages = galleryImageList
+            };
+            return View(viewModel);
+
+        }
+      
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
